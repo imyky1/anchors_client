dc3822a60d1b0b15cee65e26112594180a6eaf9e
SinghYuvraj0506 <singhyuvraj0506@gmail.com>
2022-12-06 00:58:50 +0530

    added otp feature in developer login

--- src/Developers/Login/Login.js
+++ src/Developers/Login/Login.js
@@ -1,67 +1,121 @@
-import React,{useRef,useState} from "react";
+import React, { useRef, useState,useCallback,useEffect } from "react";
 import { host } from "../../config/config";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+import "./Login.css"
 
 function Login() {
-  const ref = useRef()
-  const navigate = useNavigate()
+  const ref = useRef();
+  const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies();
+  const [timer, setTimer] = useState(60);
+  const [sentOTP,setsentOTP] = useState(false)
+ //const timeOutCallback = useCallback(
+ //   () => setTimer((currTimer) => currTimer - 1),
+ //   []
+ //);
 
-  const [data, setData] = useState({email:"",password:""})
-  
-    const handleShowPassword = () =>{
-      const doc = document.getElementById("show_pass")
-      if(doc.checked === true){
-        ref.current.type = "text"
-      }else{
-        ref.current.type = "password"
+ //const resetTimer = function () {
+    if (!timer) {
+      setTimer(60);
+    }
+ //};
+
+
+ //useEffect(() => {
+ //  timer > 0 && sentOTP && setTimeout(timeOutCallback, 1000);
+ //}, [resetTimer]);
+
+ /// console.log(timer);
+
+
+  const [data, setData] = useState({ email: "", password: "", otp:"" });
+
+  const handleShowPassword = () => {
+    const doc = document.getElementById("show_pass");
+    if (doc.checked === true) {
+      ref.current.type = "text";
+    } else {
+      ref.current.type = "password";
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${host}/api/developer/login`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({ email: data.email, password: data.password }),
+    });
+    const json = await response.json();
+    if (json.success) {
+      localStorage.setItem("jwtTokenD", json.jwtToken);
+      localStorage.setItem("isDev", true);
+      navigate("/developer/admin");
+    } else {
+      alert("Invalid Credentials Please Try Again");
+    }
+  };
+
+  const handleSubmitOTP = async (e) => {
+    e.preventDefault();
+    console.log("sent")
+    const response = await fetch(`${host}/api/email/sendMsg?message=Hello Anchorite&number=8692006538&subject=Anchors`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
       }
+    });
+    const json = await response.json();
+    if(json.MessageID){
+      setsentOTP(true)
+      let otpcode = parseInt(json.code)*562002
+      setCookie('ccoondfe', otpcode, { maxAge:60 });
     }
+  };
+  
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        const response = await fetch(`${host}/api/developer/login`, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Credentials": true,
-              },
-              body: JSON.stringify({email:data.email,password:data.password})
-          });
-          const json = await response.json();
-          if(json.success){
-            localStorage.setItem("jwtTokenD",json.jwtToken)
-            localStorage.setItem("isDev",true)
-            navigate("/developer/admin")
-          }
-          else{
-            alert("Invalid Credentials Please Try Again")
-          }
-          
+  const CheckOTP = async () =>{
+    let code = cookies?.ccoondfe
+
+    if(data.otp === (parseInt(code)/562002).toString()){
+      localStorage.setItem("jwtTokenD", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM3MGUwMmZlOWRmNjBjNTY5OGIwMGQzIn0sImlhdCI6MTY3MDI2NDMwNX0.s9J2ny1S3Pz47R8DC058kYZW-RZxXj7rsYHPzzALp6M");
+      localStorage.setItem("isDev", true);
+      navigate("/developer/admin");
+    } else {
+      alert("Invalid OTP Please Try Again");
     }
+  }
 
 
-    const handleChange = (e) =>{
-        e.preventDefault();
-        setData({...data,[e.target.name]:e.target.value})
-    }
+  const handleChange = (e) => {
+    e.preventDefault();
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="creator_login">
       <div className="creator_login_header">
-      <div className="logo">
+        <div className="logo">
           <img src={require("../../Components/logo.png")} alt="Logo" />
-          <span>Anchors</span>
-          </div>
+          <span>anchors</span>
+        </div>
       </div>
       <div className="main_page_login">
         <div className="gyan_container">
-          Hello, Anchors Builders <br/>Let's work hard and raise the level of Anchors together.
+          Hello, Anchors Builders <br />
+          Let's work hard and raise the level of Anchors together.
         </div>
-        <div className="login_container" style={{height: "79vh"}}>
-          <h2 >Welcome Back Builders</h2>
-          <img src={require("../../Components/logo2.png")} alt="logo" style={{width:"5rem",height:"5rem",marginBottom:"-50px"}} />
-          <form>
+        <div className="login_container" style={{ height: "79vh" }}>
+          <h2>Welcome Back Builders</h2>
+          {/* <img src={require("../../Components/logo2.png")} alt="logo" style={{width:"5rem",height:"5rem",marginBottom:"-50px"}} /> */}
+          <form style={{ marginTop: "25px" }}>
             <input
               className="input_cred"
               type="email"
@@ -80,13 +134,51 @@ function Login() {
               placeholder="Password"
             />
             <div className="login_check">
-            <input type="checkbox" name="show_pass" id="show_pass"  onClick={handleShowPassword}/>
-            <label htmlFor="show_pass" onClick={handleShowPassword}>Show Password</label>
+              <input
+                type="checkbox"
+                name="show_pass"
+                id="show_pass"
+                onClick={handleShowPassword}
+              />
+              <label htmlFor="show_pass" onClick={handleShowPassword}>
+                Show Password
+              </label>
             </div>
-            <input type="submit" className="login_submit" value="Login" onClick={handleSubmit} />
+            <input
+              type="submit"
+              className="login_submit"
+              value="Login"
+              onClick={handleSubmit}
+            />
           </form>
+          <h2 style={{ marginTop: "13px" }}>--------------- or ------------</h2>
+          {/* <input
+            type="submit"
+            className="login_submit"
+            value={sentOTP ? "OTP Sent" : "Login with OTP"}
+            onClick={handleSubmitOTP}
+            style={{ marginTop: "15px" }}
+          /> */}
+          <button onClick={handleSubmitOTP} className="button_login_dev">{sentOTP ? "OTP Sent" : "Login with OTP"}</button>
+
+          {sentOTP && <div className="otp">
+            <input
+              className="input_cred"
+              type="number"
+              name="otp"
+              id="otp"
+              value={data.otp}
+              placeholder="Enter OTP"
+              onChange={handleChange}
+            />
+            
+            <button onClick={CheckOTP} className="button_login_dev">Submit</button>
+            {/* <p onClick={resetTimer}>
+              Resend Code in <span className="timer">{timer + "s"}</span>
+            </p> */}
+          </div>}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from 'react-cookie';
 
 
 
@@ -10,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    
+    <CookiesProvider>
     <App />
-  
+    </CookiesProvider>
 
   </React.StrictMode>
 );
