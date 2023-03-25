afc1441448eadafa7d403464e5f98def067bde67
ggez <garvit510bhatia@gmail.com>
2023-03-26 04:22:46 +0530

    default banner

--- src/Components/Editor/New UI/Create Services/Create.js
+++ src/Components/Editor/New UI/Create Services/Create.js
@@ -230,21 +230,20 @@ function Create(props) {
 
             setTimeout(async () => {
               let canvasImage = sessionStorage.getItem("canvas");
-              console.log(canvasImage);
               // this can be used to download any image from webpage to local disk
-              let xhr = new XMLHttpRequest();
-              xhr.responseType = "blob";
-              xhr.onload = function () {
-                let a = document.createElement("a");
-                a.href = window.URL.createObjectURL(xhr.response);
-                a.download = "image_name.png";
-                a.style.display = "none";
-                document.body.appendChild(a);
-                a.click();
-                a.remove();
-              };
-              xhr.open("GET", canvasImage); // This is to download the canvas Image
-              xhr.send();
+              // let xhr = new XMLHttpRequest();
+              // xhr.responseType = "blob";
+              // xhr.onload = function () {
+              //   let a = document.createElement("a");
+              //   a.href = window.URL.createObjectURL(xhr.response);
+              //   a.download = "image_name.png";
+              //   a.style.display = "none";
+              //   document.body.appendChild(a);
+              //   a.click();
+              //   a.remove();
+              // };
+              // xhr.open("GET", canvasImage); // This is to download the canvas Image
+              // xhr.send();
               const data3 = new FormData();
 
               var blob = dataURItoBlob(canvasImage);
